5bfff20e0a493a2caeaebf16056da1c283651979
Henna <hennariina23000@student.hamk.fi>
2023-12-10 21:04:15 +0200

    Commit 3/ Jatkettu koodia ja lisätty kommentteja
    Koodiin lisätty Hennan nimi ja asuinpaikka sekä kommentteja

--- src/GitTehtava.java
+++ src/GitTehtava.java
@@ -4,9 +4,13 @@ public class GitTehtava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//Netan tiedot 
 		System.out.println("Nimeni on Netta");
 		System.out.println("Asun Tampereella");
-
+		
+		//Hennan tiedot
+		System.out.println("Nimeni on Henna");
+		System.out.println("Asun Tampereella");
 	}
 
 }
